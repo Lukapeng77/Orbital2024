33055c12f005dde40d3cffa9493e56936907a879
lukapeng77 <e1249364@u.nus.edu>
2024-08-06 23:48:42 +0800

    8th commit for MS3

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,5 @@
 
 module.exports = {
-	url: process.env.MONGODB_URI || "mongodb+srv://Luka:Pzy20020805%25@atlascluster.tzw9tmr.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster"
+	url: process.env.MONGODB_URI || 'mongodb://localhost:27017/suppeer1'
+	//"mongodb+srv://Luka:Pzy20020805%25@atlascluster.tzw9tmr.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster"
 	};
