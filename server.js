f08399518f5c187505ad2c98746a4aaffcac294d
lukapeng77 <e1249364@u.nus.edu>
2024-08-08 16:56:49 +0800

    9th commit for MS3

--- server.js
+++ server.js
@@ -61,10 +61,10 @@ app.listen(PORT, () => {
 	console.log(`Server running on port http://localhost:${PORT}`);
 });
 
-/*if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV == "production") {
 	app.use(express.static(path.join(__dirname, "/suppeer/build")));
   
 	app.get("*", (req, res) => {
 	  res.sendFile(path.join(__dirname, "suppeer/build", "index.html"));
 	});
-  }*/
+  }
