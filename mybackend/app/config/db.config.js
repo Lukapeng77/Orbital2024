fb3698d199592231b6e6392eff3a35a35d5ac704
lukapeng77 <e1249364@u.nus.edu>
2024-08-17 11:26:03 +0800

    5th commit for final submission

--- mybackend/app/config/db.config.js
+++ mybackend/app/config/db.config.js
@@ -1,5 +1,4 @@
 module.exports = {
 	url: process.env.MONGODB_URI || 'mongodb://localhost:27017/suppeer1'
-	//'mongodb+srv://pziyi86:20020805@cluster0.cfe5q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-	};
-	
+};
+
